c3dea1fcfe3c1d4893a86019e170ef67b2c3e347
Pranav Ravichandran <me@onloop.net>
2014-01-16 20:44:56 +0530

    WSGI updates

--- radar/wsgi.py
+++ radar/wsgi.py
@@ -1,14 +1,19 @@
-"""
-WSGI config for radar project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
+# test uWSGI with low traffic:
+# uwsgi --virtualenv /path/to/virtualenv --http :9090 --gevent 100 --http-websockets --module wsgi
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radar.settings")
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'radar.settings')
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.conf import settings
+from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
+
+_django_app = get_wsgi_application()
+_websocket_app = uWSGIWebsocketServer()
+
+
+def application(environ, start_response):
+    if environ.get('PATH_INFO').startswith(settings.WEBSOCKET_URL):
+        return _websocket_app(environ, start_response)
+    return _django_app(environ, start_response)
